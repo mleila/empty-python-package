a2189267556e8223df4663f4d631bec5cc9be939
Mohamed Leila <mohamed.laila@mail.mcgill.ca>
2024-03-16 15:31:47 -0400

    Update test_addition.py to import from package

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,8 +1,7 @@
-from src.mcalc.addition import add
-
+from mcalc.addition import add
 
 def test_add():
     assert add(0, 0) == 0
     assert add(2, 3) == 5
     assert add(-3, 3) == 0
-    assert add(-7, -7) == -14
+    assert add(-7, 7) == -14
