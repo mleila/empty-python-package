8e78a781189ab272050efa68cb8b32354904b99d
Mohamed Leila <mohamed.laila@mail.mcgill.ca>
2024-03-21 20:12:51 -0400

    Adding script to generate Azure Big Data platform architecture diagram

---
+++ arch_diagrams/azure_big_data_arch.py
@@ -0,0 +1,12 @@
+import azure.mgmt.resourcegraph.
+
+# you need to define a class here that generates architecture diagrams.
+
+class AzureBigDataArch:
+  def __init__(self, subscription, resource_group):
+    self.subscription = subscription
+    self.resource_group = resource_group
+
+  def generate_diagram(self):
+    # your logic to generate diagram goes here
+    pass
